c43cb5426632ca99da1bf471c864abe2b45b7b4f
henryhoang <henry.hoang@j2.com>
2015-01-08 10:52:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.debug = True
 
 @app.route('/')
 def hello():
-	return '<h1> Hey Docker from j2 Global!</h1>'
+	return '<h1> Hey Docker from j2 Cloud!</h1>'
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
